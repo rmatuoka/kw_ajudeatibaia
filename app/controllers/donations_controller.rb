f09185385eb2ec601b0e926383cb0d22581dead4
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2011-01-13 04:46:16 -0200

    Ajustes mailer

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -13,6 +13,11 @@ class DonationsController < ApplicationController
     @donation = Donation.create(params[:donation])
     
     if @donation.save
+      
+      #ENVIA EMAIL
+      @corpo = "Nome: #{@donation.nome} <br>E-mail: #{@donation.email} <br>Telefone: #{@donation.telefone_ddd} - #{@donation.telefone} <br>Mensagem: #{@donation.mensagem} <br>"
+      Notifier.deliver_enviar(@corpo.to_s,"Contato Via SITE", "contato@ajudeatibaia.com.br")
+      
       redirect_to donation_path(@donation)
     else
       render :action => "new"
